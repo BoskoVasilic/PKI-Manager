e809b08b4b63f967e0e4280e022b8e5787643465
sara-stojkov <sarastojkov6@gmail.com>
2026-04-29 19:00:16 +0200

    Fix: working login

--- backend/src/main/java/com/tim12/pk_infrastructure/PkInfrastructureApplication.java
+++ backend/src/main/java/com/tim12/pk_infrastructure/PkInfrastructureApplication.java
@@ -2,6 +2,7 @@ package com.tim12.pk_infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PkInfrastructureApplication {
@@ -9,6 +10,7 @@ public class PkInfrastructureApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PkInfrastructureApplication.class, args);
 		System.out.println("PK Infrastructure Service is running...");
+		System.out.println(new BCryptPasswordEncoder().encode("password"));
 	}
 
 }
