c93f4d0bb1fa093d9b91e21c7e9ae4836b4be3a1
Marko <milutin.sv40.2023@uns.ac.rs>
2026-04-29 21:28:00 +0200

    fix: CORS problem

--- backend/src/main/java/com/tim12/pk_infrastructure/controller/CertificateController.java
+++ backend/src/main/java/com/tim12/pk_infrastructure/controller/CertificateController.java
@@ -3,6 +3,7 @@ package com.tim12.pk_infrastructure.controller;
 import com.tim12.pk_infrastructure.dto.CertificateDto;
 import com.tim12.pk_infrastructure.service.CertificateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/certificates")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CertificateController {
 
     private final CertificateService certificateService;
@@ -27,5 +28,10 @@ public class CertificateController {
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<CertificateDto> getMyCertificate(@PathVariable String serialNumber) {
         return ResponseEntity.ok(certificateService.getMyCertificateBySerial(serialNumber));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleNotFound(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
--- backend/src/main/java/com/tim12/pk_infrastructure/service/AuthService.java
+++ backend/src/main/java/com/tim12/pk_infrastructure/service/AuthService.java
@@ -5,6 +5,7 @@ import com.tim12.pk_infrastructure.repository.UserRepository;
 import com.tim12.pk_infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class AuthService {
                     new UsernamePasswordAuthenticationToken(email, password)
             );
         } catch (AuthenticationException e) {
-            throw new RuntimeException("Invalid email or password");
+            throw new BadCredentialsException("Invalid email or password");
         }
 
         User user = userRepository.findByEmail(email)
