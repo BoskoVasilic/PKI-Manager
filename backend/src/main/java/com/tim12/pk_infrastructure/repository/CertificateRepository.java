c5f2f000f832ffcf8f3efb6b999908b700051a19
sara-stojkov <sarastojkov6@gmail.com>
2026-05-01 16:53:37 +0200

    Fix: certificate repository bug function

--- backend/src/main/java/com/tim12/pk_infrastructure/repository/CertificateRepository.java
+++ backend/src/main/java/com/tim12/pk_infrastructure/repository/CertificateRepository.java
@@ -1,6 +1,7 @@
 package com.tim12.pk_infrastructure.repository;
 
 import com.tim12.pk_infrastructure.model.Certificate;
+import com.tim12.pk_infrastructure.model.CertificateStatus;
 import com.tim12.pk_infrastructure.model.CertificateType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tim12.pk_infrastructure.model.User;
@@ -15,15 +16,13 @@ public interface CertificateRepository extends JpaRepository<Certificate, Long> 
     Optional<Certificate> findBySerialNumber(String serialNumber);
 
     // CA user: only see certs in their org
-    List<Certificate> findByOwnerOrganization(String organization);
+    List<Certificate> findByOwner(User owner);
+
+    List<Certificate> findByOwnerAndTypeInAndStatus(
+            User owner, List<CertificateType> types, CertificateStatus status);
 
     // Regular user: only their own certs
     List<Certificate> findByOwnerId(Long ownerId);
-
-    // Find available CA certs for a given org (not revoked, right type)
-    List<Certificate> findByOwnerOrganizationAndTypeInAndRevokedFalse(
-            String organization, List<CertificateType> types
-    );
 
     List<Certificate> findByOwnerAndType(User owner, CertificateType type);
     Optional<Certificate> findBySerialNumberAndOwner(String serialNumber, User owner);
