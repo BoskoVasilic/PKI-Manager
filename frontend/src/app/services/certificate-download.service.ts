2af3292eddbd139076ac9723a733f046d73333cb
sara-stojkov <sarastojkov6@gmail.com>
2026-05-18 11:49:10 +0200

    Feat: add pk certificate downloading to frontend also

--- frontend/src/app/services/certificate-download.service.ts
+++ frontend/src/app/services/certificate-download.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CertificateDto } from './certificate.service';
 
-export type DownloadFormat = 'pem' | 'cer';
+export type DownloadFormat = 'pem' | 'cer' | 'p12' | 'jks';
 
 @Injectable({ providedIn: 'root' })
 export class CertificateDownloadService {
@@ -23,6 +23,20 @@ export class CertificateDownloadService {
       responseType: 'blob',
     });
   }
+
+  downloadAsP12(serialNumber: string, exportPassword: string): Observable<Blob> {
+  return this.http.get(`${this.API_URL}/${serialNumber}/download/p12`, {
+    params: { exportPassword },
+    responseType: 'blob',
+  });
+}
+
+downloadAsJks(serialNumber: string, exportPassword: string): Observable<Blob> {
+  return this.http.get(`${this.API_URL}/${serialNumber}/download/jks`, {
+    params: { exportPassword },
+    responseType: 'blob',
+  });
+}
 
   triggerDownload(blob: Blob, filename: string): void {
     const url = URL.createObjectURL(blob);
